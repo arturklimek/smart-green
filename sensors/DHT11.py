748489ee30b5e1e7323bafa0c33f92f07bfc6d54
Artur K <artur.aklimek@gmail.com>
2024-02-12 16:19:40 +0100

    update class name

--- sensors/DHT11.py
+++ sensors/DHT11.py
@@ -4,7 +4,7 @@ import threading
 import RPi.GPIO as GPIO
 import logging
 
-class DHT11:
+class DHT11Sensor:
     """
     A singleton class to manage access to a DHT sensor. This class ensures that only one instance
     per GPIO pin is created, facilitating shared access to DHT sensors across different parts of an
@@ -14,7 +14,7 @@ class DHT11:
     a thread-safe mechanism to read humidity and temperature data from a DHT sensor.
 
     Attributes:
-        sensor_type (Adafruit_DHT.DHTxx): The type of DHT sensor (e.g., DHT11, DHT22).
+        sensor_type (Adafruit_DHT.DHT11): The type of DHT sensor
         pin (int): The GPIO pin number that the sensor is connected to.
         last_read (dict | None): The last read humidity and temperature values or None if no successful read.
         logger (logging.Logger): Logger instance for logging sensor operation messages.
@@ -25,31 +25,31 @@ class DHT11:
 
     Args:
         pin (int): The GPIO pin number where the sensor is connected.
-        sensor_type (Adafruit_DHT.DHTxx, optional): The type of DHT sensor. Defaults to Adafruit_DHT.DHT11.
+        sensor_type (Adafruit_DHT.DHT11, optional): The type of DHT sensor. Defaults to Adafruit_DHT.DHT11.
     """
-    _instances: Dict[int, 'DHT11'] = {}
+    _instances: Dict[int, 'DHT11Sensor'] = {}
     _lock: threading.Lock = threading.Lock()
 
-    sensor_type: Adafruit_DHT.DHTxx
+    sensor_type: Adafruit_DHT.DHT11
     pin: int
     last_read: Optional[Dict[str, float]] = None
     logger: logging.Logger
 
-    def __new__(cls: Type['DHT11'], pin: int, sensor_type: Adafruit_DHT.DHTxx = Adafruit_DHT.DHT11) -> 'DHT11':
+    def __new__(cls: Type['DHT11Sensor'], pin: int, sensor_type: Adafruit_DHT.DHT11 = Adafruit_DHT.DHT11) -> 'DHT11Sensor':
         """
         Ensures that only one instance of DHTSensorSingleton per GPIO pin is created. If an instance for a given pin
         already exists, it returns that instance; otherwise, it creates a new one.
 
         Args:
             pin (int): The GPIO pin number where the sensor is connected.
-            sensor_type (Adafruit_DHT.DHTxx, optional): The type of DHT sensor. Defaults to Adafruit_DHT.DHT11.
+            sensor_type (Adafruit_DHT.DHT11, optional): The type of DHT sensor.
 
         Returns:
             DHT11: An instance of the DHTSensorSingleton class for the specified GPIO pin.
         """
         with cls._lock:
             if pin not in cls._instances:
-                instance = super(DHT11, cls).__new__(cls)
+                instance = super(DHT11Sensor, cls).__new__(cls)
                 instance.sensor_type = sensor_type
                 instance.pin = pin
                 instance.last_read = None  # Type hints są już zadeklarowane na poziomie klasy
@@ -85,7 +85,7 @@ class DHT11:
         try:
             humidity, temperature = Adafruit_DHT.read_retry(self.sensor_type, self.pin)
             self.last_read = {'humidity': humidity, 'temperature': temperature}
-            self.logger.info(f"DHT on PIN: {self.pin} read {self.last_read}")
+            self.logger.info(f"DHT11 on PIN: {self.pin} read {self.last_read}")
             return self.last_read
         except Exception as e:
             self.logger.error(f"Error reading DHT sensor on pin {self.pin}: {e}")
