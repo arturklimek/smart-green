f0a8da300f878a6b2ce968eed91e1db60aa41898
Artur K <artur.aklimek@gmail.com>
2024-02-14 17:27:17 +0100

    added functionality to load and create objects (sensors, accutators, controllers) + use of configuration

--- app.py
+++ app.py
@@ -1,31 +1,370 @@
 import logging
 import os
+import time
+from typing import List, Optional, Dict, Any
+from actuators.actuator import BaseActuator
 from app_config import LOG_DIR_PATH, AppConfig, APP_CONFIG_PATH
+from controllers.controller import BaseController
+from controllers.irrigation_controller import IrrigationController
+from controllers.lighting_controller import LightingController
+from controllers.ventilation_controller import VentilationController
 from logger import LoggerManager
+from sensors.humidity_sensor import HumiditySensor
+from sensors.light_sensor import LightSensor
+from sensors.sensor import BaseSensor
+from sensors.soil_moisture_sensor import SoilMoistureSensor
+from sensors.temperature_sensor import TemperatureSensor
 
 class App:
-    def __init__(self):
-        self.setup_app()
-        self.logger = logging.getLogger('app_logger')
+    """
+    A main application class responsible for initializing and managing the entire application lifecycle.
 
-    def setup_app(self):
+    This class initializes the application's logging system, loads the configuration, manages sensors,
+    actuators, and controllers, and handles the starting and stopping of the application.
+
+    Attributes:
+        logger (logging.Logger): Application-wide logger.
+        app_config (AppConfig): Configuration manager for the application.
+        sensors (Dict[BaseSensor]): Dict of sensor objects used in the application.
+        actuators (Dict[BaseActuator]): Dict of actuator objects used in the application.
+        controllers (Dict[BaseController]): Dict of controller objects that manage the application logic.
+        running (bool): Flag indicating if the application is currently running.
+    """
+    def __init__(self) -> None:
+        """
+        Initializes the application, sets up the directory structure, initializes logging,
+        loads components based on the application configuration, and logs the initialization completion.
+        """
         self.setup_dir_structure([LOG_DIR_PATH])
         LoggerManager.setup_logger('app_logger', os.path.join(LOG_DIR_PATH, 'app.log'), level_console=logging.INFO, level_file=logging.DEBUG)
+        self.logger: logging.Logger = logging.getLogger('app_logger')
+        self.app_config: AppConfig = AppConfig(APP_CONFIG_PATH)
+        self.sensors: Dict[str, BaseSensor] = {}
+        self.actuators: Dict[str, BaseActuator] = {}
+        self.controllers: Dict[str, BaseController] = {}
+        self.load_components()
         self.logger.info('Application initialized')
-        app_config = AppConfig(APP_CONFIG_PATH)
+        self.running: bool = False
+
+    def reload_config(self) -> None:
+        """
+        Reloads the application configuration from the configuration file and logs the outcome.
+        """
+        try:
+            self.app_config.load_config()
+            self.logger.info("Configuration reloaded successfully.")
+        except Exception as ex:
+            self.logger.error(f"Failed to reload configuration: {ex}")
+
+    def run_app(self) -> None:
+        """
+        Starts the application by beginning sensor readings, starting controllers,
+        and entering the main application loop until a stop signal is received.
+        """
+        self.running = True
+        self.start_sensors_reading()
+        self.start_controllers()
+        try:
+            while self.running:
+                time.sleep(0.5)
+        finally:
+            self.stop_sensors_reading()
+            self.stop_controllers()
+
+    def stop_app(self) -> None:
+        """
+        Stops the application by signaling the main loop to terminate and waits for a short period
+        to ensure all components are properly shutdown.
+        """
+        self.running = False
+        time.sleep(4)
 
     @staticmethod
-    def setup_dir_structure(dir_list):
+    def setup_dir_structure(dir_list: List[str]) -> None:
+        """
+        Sets up the required directory structure for the application.
+
+        Args:
+            dir_list (List[str]): A list of directory paths that should be created.
+        """
         for dir_path in dir_list:
             try:
                 App.create_dir(dir_path)
             except Exception as ex:
-                print(f'Error creating directory {dir_path}: {ex}')
+                logging.error(f'Error creating directory {dir_path}: {ex}')
 
     @staticmethod
-    def create_dir(dir_path: str):
+    def create_dir(dir_path: str) -> None:
+        """
+        Creates a directory if it does not already exist.
+
+        Args:
+            dir_path (str): Path of the directory to be created.
+        """
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
+            logging.info(f"Directory created: {dir_path}")
 
-    def start_app(self):
-        pass
+    def start_controllers(self) -> None:
+        """
+        Initiates the start sequence for all configured controllers within the application.
+        """
+        for name, controller in self.controllers.items():
+            try:
+                if controller:
+                    controller.start()
+                    self.logger.info(f"{controller.__class__.__name__} started.")
+            except Exception as ex:
+                self.logger.error(f"Failed to start {controller.__class__.__name__}: {ex}")
+
+    def stop_controllers(self) -> None:
+        """
+        Initiates the stop sequence for all running controllers within the application.
+        """
+        for name, controller in self.controllers.items():
+            try:
+                if controller:
+                    controller.stop()
+                    self.logger.info(f"{controller.__class__.__name__} stopped.")
+            except Exception as ex:
+                self.logger.error(f"Failed to stop {controller.__class__.__name__}: {ex}")
+
+    def start_sensors_reading(self) -> None:
+        """
+        Starts the data reading process for all configured sensors.
+        """
+        for name, sensor in self.sensors.items():
+            try:
+                if sensor:
+                    sensor.start_reading()
+                    self.logger.info(f"{sensor.__class__.__name__} reading started.")
+            except Exception as ex:
+                self.logger.error(f"Failed to start reading {sensor.__class__.__name__}: {ex}")
+
+    def stop_sensors_reading(self) -> None:
+        """
+        Stops the data reading process for all sensors that are currently reading data.
+        """
+        for name, sensor in self.sensors.items():
+            try:
+                if sensor:
+                    sensor.stop_reading()
+                    self.logger.info(f"{sensor.__class__.__name__} stopped reading.")
+            except Exception as ex:
+                self.logger.error(f"Failed to stop reading {sensor.__class__.__name__}: {ex}")
+
+    def load_components(self) -> None:
+        """
+        Loads sensors, actuators, and controllers based on the application configuration.
+        Adds successfully created components to their respective lists within the application.
+        """
+        config: Dict[str, Any] = self.app_config.get_config()
+        try:
+            for sensor_config in config.get('sensors', []):
+                name = sensor_config['name']
+                sensor = self.create_sensor(sensor_config)
+                if sensor:
+                    self.sensors[name] = sensor
+
+            for actuator_config in config.get('actuators', []):
+                name = actuator_config['name']
+                actuator = self.create_actuator(actuator_config)
+                if actuator:
+                    self.actuators[name] = actuator
+
+            for controller_config in config.get('controllers', []):
+                name = controller_config.get('name', '')
+                controller = self.create_controller(controller_config)
+                if controller:
+                    self.controllers[name] = controller
+        except Exception as ex:
+            self.logger.error(f"Error loading components: {ex}")
+
+    def create_sensor(self, sensor_config: Dict[str, Any]) -> Optional[BaseSensor]:
+        """
+        Creates a sensor object based on the validated configuration.
+
+        Args:
+            sensor_config (Dict[str, Any]): Configuration dictionary for the sensor.
+
+        Returns:
+            Optional[BaseSensor]: The created sensor object or None if creation failed due to invalid configuration.
+        """
+        try:
+            sensor_type = sensor_config['type']
+            params = sensor_config.get('params', {})
+            sensor_params = {
+                'BaseSensor': {'optional': ['read_frequency', 'max_readings', 'start_immediately', 'anomaly_detection'],
+                               'required': []},
+                'TemperatureSensor': {
+                    'optional': ['max_value', 'min_value', 'read_frequency', 'max_readings', 'start_immediately',
+                                 'anomaly_detection'], 'required': ['pin']},
+                'HumiditySensor': {
+                    'optional': ['max_value', 'min_value', 'read_frequency', 'max_readings', 'start_immediately',
+                                 'anomaly_detection'], 'required': ['pin']},
+                'LightSensor': {'optional': ['i2c_address', 'min_value', 'max_value', 'read_frequency', 'max_readings',
+                                             'start_immediately', 'anomaly_detection'], 'required': []},
+                'SoilMoistureSensor': {
+                    'optional': ['i2c_address', 'channel', 'min_value', 'max_value', 'read_frequency', 'max_readings',
+                                 'start_immediately', 'anomaly_detection'], 'required': []}
+            }
+            valid_params = {}
+            if sensor_type in sensor_params:
+                for param in params:
+                    if param in sensor_params[sensor_type]['required'] + sensor_params[sensor_type]['optional']:
+                        if isinstance(params[param], (int, float, bool)) or params[param] is None:
+                            valid_params[param] = params[param]
+                        else:
+                            self.logger.warning(f"Incorrect type for {param} in {sensor_type}, using default value.")
+                    else:
+                        self.logger.warning(f"Unknown parameter {param} for {sensor_type}, ignoring it.")
+                missing_required = [r for r in sensor_params[sensor_type]['required'] if r not in valid_params]
+                if missing_required:
+                    self.logger.error(f"Missing required parameters {missing_required} for {sensor_type}.")
+                    return None
+                try:
+                    sensor_class = eval(sensor_type)
+                    return sensor_class(**valid_params)
+                except Exception as ex:
+                    self.logger.error(f"Failed to create sensor {sensor_type} with params {valid_params}: {ex}")
+                    return None
+            else:
+                self.logger.error(f"Unknown sensor type: {sensor_type}")
+                return None
+        except Exception as ex:
+            self.logger.error(f"Can not create sensor: {ex}")
+
+    def create_actuator(self, actuator_config: Dict[str, Any]) -> Optional[BaseActuator]:
+        """
+        Creates an actuator object based on the validated configuration.
+
+        Args:
+            actuator_config (Dict[str, Any]): Configuration dictionary for the actuator.
+
+        Returns:
+            Optional[BaseActuator]: The created actuator object or None if creation failed due to invalid configuration.
+        """
+        try:
+            actuator_type = actuator_config['type']
+            params = actuator_config.get('params', {})
+            required_params = ['gpio_pin']
+            optional_params = ['initial_state']
+            valid_params = {}
+            if actuator_type == 'BaseActuator':
+                for param in params:
+                    if param in required_params + optional_params:
+                        if param == 'gpio_pin' and isinstance(params[param], int):
+                            valid_params[param] = params[param]
+                        elif param == 'initial_state' and isinstance(params[param], bool):
+                            valid_params[param] = params[param]
+                        else:
+                            self.logger.warning(f"Incorrect type for {param} in {actuator_type}, using default value.")
+                    else:
+                        self.logger.warning(f"Unknown parameter {param} for {actuator_type}, ignoring it.")
+                missing_required = [r for r in required_params if r not in valid_params]
+                if missing_required:
+                    self.logger.error(f"Missing required parameters {missing_required} for {actuator_type}.")
+                    return None
+                try:
+                    return BaseActuator(**valid_params)
+                except Exception as ex:
+                    self.logger.error(f"Failed to create actuator {actuator_type} with params {valid_params}: {ex}")
+                    return None
+            else:
+                self.logger.error(f"Unknown actuator type: {actuator_type}")
+                return None
+        except Exception as ex:
+            self.logger.error(f"Can not create actuator: {ex}")
+
+    def create_controller(self, controller_config: Dict[str, Any]) -> Optional[BaseController]:
+        """
+        Creates a controller object based on the validated configuration, linking it with its sensors and actuator.
+
+        Args:
+            controller_config (Dict[str, Any]): Configuration dictionary for the controller.
+
+        Returns:
+            Optional[BaseController]: The created controller object or None if creation failed due to invalid configuration or missing components.
+        """
+        try:
+            controller_type = controller_config['type']
+            sensor_names = controller_config.get('sensors', {})
+            actuator_name = controller_config.get('actuator')
+            params = controller_config.get('params', {})
+            sensors_list = []
+            for sensor_name, sensor_info in sensor_names.items():
+                sensor = self.sensors.get(sensor_name)
+                if sensor:
+                    sensor_info_validated = {
+                        'sensor': sensor,
+                        'threshold': sensor_info.get('threshold'),
+                        'comparison': BaseController.get_comparison(sensor_info.get('comparison')),
+                    }
+                    sensors_list.append(sensor_info_validated)
+                else:
+                    self.logger.error(f"Sensor '{sensor_name}' not found.")
+                    return None
+            actuator = self.actuators.get(actuator_name)
+            if not actuator:
+                self.logger.error(f"Actuator '{actuator_name}' not found.")
+                return None
+            if controller_type == 'IrrigationController':
+                irrigation_params = {
+                    'pump_flow_rate': params.pop('pump_flow_rate', None),
+                    'water_volume': params.pop('water_volume', None),
+                    'max_lift_height': params.pop('max_lift_height', None),
+                    'current_lift_height': params.pop('current_lift_height', None),
+                }
+                if irrigation_params['pump_flow_rate'] is not None and irrigation_params['water_volume'] is not None:
+                    activation_time = IrrigationController.calculate_pump_activation_time(**irrigation_params)
+                    params['activation_time'] = activation_time
+            try:
+                if controller_type in ['IrrigationController', 'VentilationController', 'LightingController']:
+                    controller_class = eval(controller_type)
+                    controller_instance = controller_class(sensors=sensors_list, actuator=actuator, **params)
+                    return controller_instance
+                else:
+                    self.logger.error(f"Unknown controller type: {controller_type}")
+                    return None
+            except Exception as ex:
+                self.logger.error(f"Failed to create controller {controller_type} with error: {ex}")
+                return None
+        except Exception as ex:
+            self.logger.error(f"Can not create controller: {ex}")
+
+    def get_sensor_by_name(self, name: str) -> Optional[BaseSensor]:
+        """
+        Retrieves a sensor object by its name from the list of configured sensors.
+
+        Args:
+            name (str): The name identifier for the sensor.
+
+        Returns:
+            Optional[BaseSensor]: The sensor object if found, None otherwise.
+        """
+        return self.sensors.get(name, None)
+
+    def get_actuator_by_name(self, name: str) -> Optional[BaseActuator]:
+        """
+        Retrieves an actuator object by its name from the list of configured actuators.
+
+        Args:
+            name (str): The name identifier for the actuator.
+
+        Returns:
+            Optional[BaseActuator]: The actuator object if found, None otherwise.
+        """
+        return self.actuators.get(name, None)
+
+    def __del__(self) -> None:
+        """
+        Destructor method to ensure all application resources are properly cleaned up before the application exits.
+        Stops the application, sensors, and controllers, and logs the cleanup process.
+        """
+        try:
+            self.stop_app()
+            self.stop_sensors_reading()
+            self.stop_controllers()
+            self.logger.info("Application resources have been cleaned up.")
+        except Exception as ex:
+            self.logger.error(f"Error during application cleanup: {ex}")
